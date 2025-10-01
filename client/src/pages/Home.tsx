ce1a84c12f045aff1f86cca62d0d6332e1332d79
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-10-01 21:48:30 +0600

    Big Banner animation Change

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -11,71 +11,6 @@ import { Separator } from "../components/ui/separator";
 
 export default function Home() {
   const { products } = useProductContext();
-//   const products = {
-//   id: 740683,
-//   name: "Nike Air Max Invigor 'Black' 740683",
-//   sku: "BWRD-WATCH-CAPITAL",
-//   brand: "Brand: Watch Capital",
-//   rating: 4.5,
-//   reviewCount: 17,
-//   price: 120.0,
-//   salePrice: 79.99,
-//   shortDescription:
-//     "Elevate your look with the modern, minimalist timepiece. Durable construction and quartz movement ensure timeless style and reliability.",
-//   stock: 150,
-//   sizes: ["Small", "Medium", "Large"],
-//   colors: [
-//     { name: "White/Rose Gold", hex: "#f8fafc" },
-//     { name: "Black/Silver", hex: "#1f2937" },
-//     { name: "Olive Green", hex: "#4b5563" },
-//   ],
-//   images: [
-//     "https://demos.codezeel.com/prestashop/PRS21/PRS210502/123-home_default/hummingbird-notebook.jpg",
-//     "https://placehold.co/500x500/e0e0e0/333?text=Watch+2",
-//     "https://placehold.co/500x500/d0d0d0/333?text=Watch+3",
-//     "https://placehold.co/500x500/c0c0c0/333?text=Watch+4",
-//   ],
-//   descriptionContent: [
-//     {
-//       title: "The Best Match",
-//       text: "The best tool to compliment your look with a watch that never goes out of style. This aesthetic, sophisticated piece will look great on your desk, or during a special night out. Paired nicely with denim or dress pants for a more elegant look.",
-//     },
-//     {
-//       title: "Lightweight Design",
-//       text: "Designed with a super light aerospace-grade metal frame, this watch is surprisingly light, giving you comfort while maintaining a sense of luxury all day long. Built to absorb hard shocks, it offers a solid and secure accessory for everyday use. Its thin profile is also suitable for smaller wrists.",
-//     },
-//   ],
-//   productDetailsContent: [
-//     {
-//       title: "Free Shipping & Returns",
-//       text: "We offer free shipping for all products on orders above $50 and include free delivery for all orders in US/Canada. Orders usually ship within 24 hours.",
-//     },
-//     {
-//       title: "Worry-Free Guarantee",
-//       text: "We guarantee our products and service, offering a full refund or exchange within 30 days of purchase.",
-//     },
-//     {
-//       title: "Online Support",
-//       text: "You can reach our team via chat or email, 24/7. We usually respond within an hour, even on weekends.",
-//     },
-//   ],
-//   reviewsContent: [
-//     {
-//       title: "asefasdfsadfasdf",
-//       text: "We offer free shippifasdfaasdfsdfngasd for all pfasdfroducts on ordasdfasdfasdove $50 and include free delivery forasdasdff all orders in US/Canada. Orders usually ship within 24 hours.",
-//     },
-//     {
-//       title: "Worry-Free dfgsdfgsdfg",
-//       text: "We guarantee our productss aasf",
-//     },
-//     {
-//       title: "Online Support",
-//       text: "You can reach our team via chat or email, 24/7. We usually respond within an hour, even on weekends.",
-//     },
-//   ],
-// };
-
-
   return (
     <div className="flex flex-col bg-muted items-center justify-center">
       {/* Home Page 1 */}
@@ -171,14 +106,17 @@ export default function Home() {
 
       {/* Home Page 5 Use Big Banner */}
       <div className="grid grid-cols-1 md:grid-cols-2 overflow-hidden gap-5 container-sm md:container-md">
-        <div data-aos="slide-up" data-aos-delay="200" data-aos-duration="400">
+        <div
+          data-aos="slide-right"
+          data-aos-delay="200"
+          data-aos-duration="400">
           <BigBanner
             image="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/cms/cms-banner-4.jpg"
             bannerStatus="Save Up To 20% Off"
             title="Santa Lucia Three Seater Sofa"
           />
         </div>
-        <div data-aos="slide-down" data-aos-delay="400" data-aos-duration="600">
+        <div data-aos="slide-left" data-aos-delay="400" data-aos-duration="600">
           <BigBanner
             image="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/cms/cms-banner-5.jpg"
             bannerStatus="Best Online Discount"
@@ -212,7 +150,10 @@ export default function Home() {
 
       {/* Home Page 9 Big Banner */}
       <div className="grid grid-cols-1 md:grid-cols-2 overflow-hidden gap-5 container-sm md:container-md">
-        <div data-aos="slide-up" data-aos-delay="200" data-aos-duration="400">
+        <div
+          data-aos="slide-right"
+          data-aos-delay="200"
+          data-aos-duration="400">
           <BigBanner
             image="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/cms/cms-banner-6.jpg"
             bannerStatus="20 Days Return Products"
