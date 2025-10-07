2b8e61103d8baa4c71f50c1c40d2ee486a2a1dc5
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-10-07 19:02:36 +0600

    Vercel Cors Origin Set

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mern-stact-ecommerce-app.vercel.app",
+    ],
     credentials: true,
   })
 );
